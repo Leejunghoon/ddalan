498c96a19da0503956f6b6b92362a108038c1c9a
seulilee <esrie@naver.com>
2014-08-13 11:58:07 +0900

    SettingsTab.java 수정, ProfilePref 추가

---
+++ DdalanO/src/com/ddalan/www/ProfilePref.java
@@ -0,0 +1,15 @@
+package com.ddalan.www;
+
+
+import android.content.Context;
+import android.preference.DialogPreference;
+import android.util.AttributeSet;
+
+public class ProfilePref extends DialogPreference 
+{
+    public ProfilePref(Context oContext, AttributeSet attrs)
+    {
+        super(oContext, attrs);
+        
+    }
+}
--- DdalanO/src/com/ddalan/www/SettingsTab.java
+++ DdalanO/src/com/ddalan/www/SettingsTab.java
@@ -1,31 +1,29 @@
 package com.ddalan.www;
 
-
-import android.app.Activity;
 import android.os.Bundle;
-
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceFragment;
 
+public class SettingsTab extends PreferenceFragment{
 
-public class SettingsTab extends PreferenceFragment {
+//	SharedPreferences.Editor editor;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		addPreferencesFromResource(R.layout.settingstab);		
+		addPreferencesFromResource(R.layout.settingstab);
+		
+		Preference avatarPref=(Preference)findPreference("setAvatar");
+		avatarPref.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+			
+			@Override
+			public boolean onPreferenceClick(Preference preference) {
+
+				return false;
+			}
+		});
+
 	}
-	
-public class SettingsActivity extends Activity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
-        // Display the fragment as the main content.
-        getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingsTab())
-                .commit();
-	    }
-	}	
 }
-
